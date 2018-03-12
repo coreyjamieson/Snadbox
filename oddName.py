af8115ce0a5db7bd5147055fb3b65eca970f4a8e
CoreyJamieson <corey.jamieson@my.jcu.edu.au>
2018-03-13 09:29:55 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,11 @@
 """
 Corey Jamieson
 """
+
+try:
+    name = str(input("Please enter name"))
+    while name == ():
+        name = str(input("Please enter valid name"))
+    print(name[0:2])
+except ValueError:
+    print("Please enter valid name")
